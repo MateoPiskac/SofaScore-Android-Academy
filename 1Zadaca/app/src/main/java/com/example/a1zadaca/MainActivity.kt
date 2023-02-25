a71b7b0ff531358ab47fbd595e2195c21fc27936
Mateo Piskac <mateopiskac2@gmail.com>
2023-02-25 15:12:00 +0100

    Initial commit

---
+++ 1Zadaca/app/src/main/java/com/example/a1zadaca/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.a1zadaca
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
