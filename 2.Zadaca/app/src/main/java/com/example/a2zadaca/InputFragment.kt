c27439d9bd8c7989b7d682e0b9e190b6de10f1a5
Mateo Piskac <mateopiskac2@gmail.com>
2023-03-07 11:15:35 +0100

    Code cleanup

--- 2.Zadaca/app/src/main/java/com/example/a2zadaca/InputFragment.kt
+++ 2.Zadaca/app/src/main/java/com/example/a2zadaca/InputFragment.kt
@@ -5,40 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
 import androidx.lifecycle.ViewModelProvider
+import com.example.a2zadaca.databinding.FragmentInputBinding
 import com.example.a2zadaca.ui.main.PageViewModel
 import com.example.a2zadaca.ui.main.Person
 
 class InputFragment : Fragment(R.layout.fragment_input) {
-    private lateinit var addButton: Button
-    private lateinit var nameEditText: EditText
-    private lateinit var surnameEditText: EditText
-    private lateinit var ageEditText: EditText
-    private lateinit var oibEditText: EditText
+    private lateinit var binding: FragmentInputBinding
     private lateinit var viewModel: PageViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_input, container, false)
-        addButton = view.findViewById(R.id.add_button)
-        nameEditText = view.findViewById(R.id.editTextName)
-        surnameEditText = view.findViewById(R.id.editTextSurname)
-        ageEditText = view.findViewById(R.id.editTextAge)
-        oibEditText = view.findViewById(R.id.editTextOIB)
+        binding = FragmentInputBinding.inflate(layoutInflater)
         viewModel = ViewModelProvider(requireActivity())[PageViewModel::class.java]
-        addButton.setOnClickListener {
+        binding.addButton.setOnClickListener {
             viewModel.addPerson(Person(
-                nameEditText.text.toString(),
-                surnameEditText.text.toString(),
-                ageEditText.text.toString(),
-                oibEditText.text.toString(),
+                binding.editTextName.text.toString(),
+                binding.editTextSurname.text.toString(),
+                binding.editTextAge.text.toString(),
+                binding.editTextOIB.text.toString(),
             ))
-            nameEditText.text.clear()
-            surnameEditText.text.clear()
-            ageEditText.text.clear()
-            oibEditText.text.clear()
+            binding.editTextName.text.clear()
+            binding.editTextSurname.text.clear()
+            binding.editTextAge.text.clear()
+            binding.editTextOIB.text.clear()
         }
-        return view
+        return binding.root
     }
 }
--- 2.Zadaca/app/src/main/java/com/example/a2zadaca/ListFragment.kt
+++ 2.Zadaca/app/src/main/java/com/example/a2zadaca/ListFragment.kt
@@ -9,14 +9,15 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.a2zadaca.databinding.FragmentListBinding
 import com.example.a2zadaca.ui.main.PageViewModel
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 
 
 class ListFragment : Fragment(R.layout.fragment_list) {
+    private lateinit var binding: FragmentListBinding
     private lateinit var pageViewModel: PageViewModel
-    private lateinit var recyclerView: RecyclerView
     private lateinit var personAdapter: PersonAdapter
     override fun onStart() {
         super.onStart()
@@ -32,17 +33,15 @@ class ListFragment : Fragment(R.layout.fragment_list) {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_list, container, false)
-        recyclerView = view.findViewById(R.id.recycler_view)
-        recyclerView.layoutManager = LinearLayoutManager(context)
-
-        return view
+        binding = FragmentListBinding.inflate(layoutInflater)
+        binding.recyclerView.layoutManager = LinearLayoutManager(context)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         personAdapter = PersonAdapter()
-        recyclerView.adapter=personAdapter
+        binding.recyclerView.adapter=personAdapter
     }
 
 }
